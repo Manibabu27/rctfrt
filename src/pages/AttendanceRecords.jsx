92aeefe4c80212308dfe74b3631725301bbecffa
Manibabu2710 <psnaidu98765@gmail.com>
2026-02-25 16:28:50 +0530

    bonus -1

--- src/pages/AttendanceRecords.jsx
+++ src/pages/AttendanceRecords.jsx
@@ -1,23 +1,45 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { getAttendance } from "../services/api";
+import { useParams } from "react-router-dom";
 
 function AttendanceRecords() {
-  const { id } = useParams();
-  const [records, setRecords] = useState([]);
+  const { id } = useParams(); // employee_id
+  const [attendance, setAttendance] = useState([]);
+  const [selectedDate, setSelectedDate] = useState("");
 
   useEffect(() => {
-    const fetchData = async () => {
+    fetchAttendance();
+  }, []);
+
+  const fetchAttendance = async () => {
+    try {
       const res = await getAttendance(id);
-      setRecords(res.data);
-    };
-    fetchData();
-  }, [id]);
+      setAttendance(res.data);
+    } catch (err) {
+      alert("Failed to fetch attendance");
+    }
+  };
+
+  // Filter logic
+  const filteredAttendance = selectedDate
+  ? attendance.filter(
+      (record) =>
+        record.date.slice(0, 10) === selectedDate
+    )
+  : attendance;
 
   return (
     <div>
       <h2>Attendance Records</h2>
-      <table border="1" cellPadding="10">
+
+      <label>Select Date To Filtre: </label>
+      <input
+        type="date"
+        value={selectedDate}
+        onChange={(e) => setSelectedDate(e.target.value)}
+      />
+
+      <table border="1" cellPadding="10" style={{ marginTop: "20px" }}>
         <thead>
           <tr>
             <th>Date</th>
@@ -25,12 +47,18 @@ function AttendanceRecords() {
           </tr>
         </thead>
         <tbody>
-          {records.map((rec, index) => (
-            <tr key={index}>
-              <td>{rec.date}</td>
-              <td>{rec.status}</td>
+          {filteredAttendance.length > 0 ? (
+            filteredAttendance.map((record, index) => (
+              <tr key={index}>
+                <td>{record.date}</td>
+                <td>{record.status}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="2">No records found</td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
